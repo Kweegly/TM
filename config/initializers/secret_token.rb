63ac7aa275496a34f3a7e2d862bf917fef2b9951
zeroc007 <bob2005uk@hotmail.co.uk>
2014-03-28 23:49:06 +0000

    Initial upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TradersmateCom::Application.config.secret_key_base = '9fd983ffd763e1424f8668b23d781444caffe34ecdec32d545f168388ef733b874fbf2a3b9cc1b2e1362b0091cf4bda99b832e12b795cb1abc67bc78be06f327'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TradersmateCom::Application.config.session_store :cookie_store, key: '_tradersmate_com_session'
